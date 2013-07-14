73dc933b1052573f07b83fd7144ae9563a783a7c
Michael Keller <michael.f.keller@bluewin.ch>
2013-07-15 00:23:19 +0200

    chater 10 end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,11 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
   private
 
     def signed_in_user
@@ -63,4 +68,6 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
+
+    
 end
