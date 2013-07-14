5d0769842a070a8e6aab006ff4da20965e98bccd
Michael Keller <michael.f.keller@bluewin.ch>
2013-07-14 20:42:46 +0200

    end chapter 9

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Keller"
-    email    "michael.keller@access.uzh.ch"
-    password "666666"
-    password_confirmation "666666"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+    factory :admin do
+      admin true
+    end
   end
 end
